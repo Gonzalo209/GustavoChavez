70ea172ebab54edee8d0f8ab6d482cbc80a6b8fc
Gonzalo209 <79113842+Gonzalo209@users.noreply.github.com>
2021-04-26 20:40:42 -0500

    Update divisas.c

--- divisas.c
+++ divisas.c
@@ -1,5 +1,5 @@
 /*
-	Crear cÛdigo fuente (divisas.c), el programa debe pedir una cantidad al usuario 
+	Crear c√≥digo fuente (divisas.c), el programa debe pedir una cantidad al usuario 
 	y convertirla a alguna de las divisas disponibles (euro, dolar estadounidense o libra).
 	
 */
@@ -13,31 +13,34 @@ DEFINIR UN VALOR CONSTANTE
 PARA QUE NO LO ALTEREMOS.
 -----------------------------
 */
-#define pesos1 23.99
-#define pesos2 19.81
-#define pesos3 27.55
-
+#define PESOS1 23.99
+#define PESOS2 19.81
+#define PESOS3 27.55
+#define EUROS 0.042
+#define DOLARES 0.050
+#define LIBRAS 0.036
 
 int main(){
 	
 	//VARIABLES LOCALES.
 	
-	float euros = 0.0, conversion = 0.0, dolares = 0.0, libras = 0.0;
+	float euros = 0.0, conversion = 0.0, dolares = 0.0, libras = 0.0, pesos = 0.0;
 	int divisas;
-	char tecla;
+	char tecla, convertir;
 	
 	do{
 		
 		/*
 		---------------------------------------
-		REALIZAMOS NUESTRO MEN⁄ DE 4 OPCIONES
+		REALIZAMOS NUESTRO MEN√ö DE 4 OPCIONES
 		DEL CONVERSOR DE MONEDAS
 		--------------------------------------
 		*/
 		printf("\n\n\t\t--------------------------------------------------\n");
-		printf("\t\t\t\tCONVERSOR DE MONEDAS");
+		printf("\t\t\t\tCONVERSOR DE MONEDAS                            ");
 		printf("\n\t\t--------------------------------------------------\n");	
-		printf("\n\n1.Conversor de euros a pesos\n2.Conversos de dolar estadounidense a pesos\n3.Conversor de libras a pesos\n4.Salir");
+		printf("\n\n1.Conversor de euros a pesos o pesos a euros\n2.Conversos de dolar estadounidense a pesos o de pesos a dolar estadounidense"
+		" \n3.Conversor de libras a pesos o de pesos a libras\n4.Salir");
 		printf("\nEliga una opcion, por favor: ");
 		scanf("%d", &divisas);
 		
@@ -45,14 +48,51 @@ int main(){
 			
 			case 1:
 				
-				printf("\n\n\tConversor de euros a pesos");
+				printf("\n\n\t\t---------------------------------------------------------\n");
+				printf("\t\t\tCONVERSOR EUROS A PESOS O DE PESOS A EUROS          ");
+				printf("\n\t\t-----------------------------------------------------------\n");
+				
+				printf("Si desea hacer la conversion de euros a pesos, introduca una [e], por favor: \n");
+				printf("Si desea hacer la conversion de pesos a euros, introduca una [p], por favor: \n");
+				printf("\nIngrese la letra [e] o [p]: ");
+				scanf("%s", &convertir);
+				
+				if(convertir == 'e'){
+				
+				printf("\n\n\t\t--------------------------------------------------\n");
+				printf("\t\t\t\tCONVERSOR DE EUROS A PESOS                            ");
+				printf("\n\t\t--------------------------------------------------\n");
 				
 				printf("\nIngrese el total de euros a cambiar: ");
 				scanf("%f", &euros);
 				
-				conversion = euros * pesos1;
+				conversion = euros * PESOS1;
 				
 				printf("\nLa conversion de euros a pesos es de: \n\n %.2f euros = %.2f pesos", euros, conversion);
+				
+				}
+				
+				else
+					if(convertir == 'p'){
+					
+				printf("\n\n\t\t--------------------------------------------------\n");
+				printf("\t\t\t\tCONVERSOR DE PESOS A EUROS                            ");
+				printf("\n\t\t--------------------------------------------------\n");
+				
+				printf("\nIngrese el total de pesos a cambiar: ");
+				scanf("%f", &pesos);
+				
+				conversion = pesos * EUROS;
+				
+				printf("\nLa conversion de pesos a euros es de: \n\n %.2f pesos = %.2f euros", euros, conversion);	
+					
+				}
+				
+				else{
+					
+					printf("\nError");
+					
+				}
 				
 				printf("\nPresione la tecla s o S, por favor: ");
 				scanf("%s", &tecla);
@@ -68,14 +108,52 @@ int main(){
 				
 			case 2:
 				
-				printf("\n\n\tConversor de dolares estadounidenses a pesos");
+				printf("\n\n\t\t---------------------------------------------------------\n");
+				printf("\t\t\tCONVERSOR DOLARES A PESOS O DE PESOS A DOLARES          ");
+				printf("\n\t\t-----------------------------------------------------------\n");
+				
+				printf("Si desea hacer la conversion de dolares a pesos, introduca una [d], por favor: \n");
+				printf("Si desea hacer la conversion de pesos a euros, introduca una [p], por favor: \n");
+				printf("\nIngrese la letra [d] o [p]: ");
+				scanf("%s", &convertir);
+				
+				if(convertir == 'd'){
+				
+				
+				printf("\n\n\t\t--------------------------------------------------\n");
+				printf("\t\t\t\tCONVERSOR DE DOLARES A PESOS                            ");
+				printf("\n\t\t--------------------------------------------------\n");
 				
 				printf("\nIngrese el total de dolares a cambiar: ");
 				scanf("%f", &dolares);
 				
-				conversion = dolares * pesos2;
+				conversion = dolares * PESOS2;
 				
-				printf("La conversion de euros a pesos es de: \n\n %.2f euros = %.2f pesos", dolares, conversion);
+				printf("La conversion de dolares a pesos es de: \n\n %.2f dolares = %.2f pesos", dolares, conversion);
+				
+				}
+				
+				else
+					if(convertir == 'p'){
+					
+				printf("\n\n\t\t--------------------------------------------------\n");
+				printf("\t\t\t\tCONVERSOR DE PESOS A DOLARES                            ");
+				printf("\n\t\t--------------------------------------------------\n");
+				
+				printf("\nIngrese el total de pesos a cambiar: ");
+				scanf("%f", &pesos);
+				
+				conversion = pesos * DOLARES;
+				
+				printf("La conversion de pesos a dolares es de: \n\n %.2f pesos = %.2f dolares", pesos, conversion);
+					
+				}
+				
+				else{
+					
+					printf("\nError");
+					
+				}
 				
 				printf("\nPresione la tecla s o S, por favor: ");
 				scanf("%s", &tecla);
@@ -90,14 +168,53 @@ int main(){
 				
 			case 3:
 			
-				printf("\n\n\tConversor de libras a pesos");
+				printf("\n\n\t\t---------------------------------------------------------\n");
+				printf("\t\t\tCONVERSOR LIBRAS A PESOS O DE PESOS A LIBRAS          ");
+				printf("\n\t\t-----------------------------------------------------------\n");
+				
+				printf("Si desea hacer la conversion de libras a pesos, introduca una [l], por favor: \n");
+				printf("Si desea hacer la conversion de pesos a libras, introduca una [p], por favor: \n");
+				printf("\nIngrese la letra [d] o [p]: ");
+				scanf("%s", &convertir);
+				
+				if(convertir == 'l'){
+						
+				printf("\n\n\t\t--------------------------------------------------\n");
+				printf("\t\t\t\tCONVERSOR DE LIBRAS A PESOS                            ");
+				printf("\n\t\t--------------------------------------------------\n");
 				
 				printf("\nIngrese el total de libras a cambiar: ");
 				scanf("%f", &libras);
 				
-				conversion = libras * pesos3;
+				conversion = libras * PESOS3;
 				
-				printf("La conversion de libras a pesos es de: \n\n %.2f euros = %.2f pesos", libras, conversion);
+				printf("La conversion de libras a pesos es de: \n\n %.2f libras = %.2f pesos", libras, conversion);
+				
+				}
+				
+				else
+					if(convertir == 'p'){
+						
+					printf("\n\n\t\t--------------------------------------------------\n");
+					printf("\t\t\t\tCONVERSOR DE PESOS A LIBRAS                            ");
+					printf("\n\t\t--------------------------------------------------\n");
+				
+					printf("\nIngrese el total de pesos a cambiar: ");
+					scanf("%f", &pesos);
+				
+					conversion = pesos * LIBRAS;
+				
+					printf("La conversion de pesos a libras es de: \n\n %.2f pesos = %.2f libras", pesos, conversion);	
+					
+					
+					
+				}
+				
+				else{
+					
+					printf("\nError");
+					
+				}
 				
 				printf("\nPresione la tecla s o S, por favor: ");
 				scanf("%s", &tecla);
@@ -141,9 +258,9 @@ int main(){
 			
 		}
 		
-	}while(divisas != 4); //HASTA QUE NOSOTROS DIGITEMOS EL N⁄MERO 4 SE SALDR¡ DEL SISTEMA.
+	}while(divisas != 4); //HASTA QUE NOSOTROS DIGITEMOS EL N√öMERO 4 SE SALDR√Å DEL SISTEMA.
 	
 	
 	
 	return 0;
-}
+} 	
